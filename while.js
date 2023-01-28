b929ea29230a8921cb2a32076d465fcdd2622253
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-28 14:33:08 +0600

    add to js while loop part

--- while.js
+++ while.js
@@ -1,5 +1,19 @@
 var number = 0;
 while (number < 10) {
-    console.log('ami vallo manos', number);
+    // console.log('ami vallo manos', number);
     number++
 }
+
+var names = ['Sani', 'Raju', 'Emamol', 'romjan'];
+// console.log(names.length);
+// for (var i = 0; i < names.length; i++) {
+//     console.log(names[i])
+// }
+var i = 0;
+while (i < names.length) {
+    console.log(names[i]);
+    i++;
+}
+
+
+
