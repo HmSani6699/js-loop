b5a4944b05bd023500987ddea8da8fdf4cd0e2a6
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-28 15:24:08 +0600

    ad to js brack part

---
+++ brack.js
@@ -0,0 +1,9 @@
+var num = [20, 40, 30, 19, 10, 70];
+
+for (var i = 0; i < num.length; i++) {
+    if (num[i] < 20) {
+        // console.log(num[i])
+        continue;
+    }
+    console.log(num[i])
+}
