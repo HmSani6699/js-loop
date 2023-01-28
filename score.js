9f9fa004cebdc1eeb98044a5a231c4538bb5d1c1
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-28 20:37:11 +0600

    ad to friends resuld score

---
+++ score.js
@@ -0,0 +1,17 @@
+var totalScore = 40;
+
+if (totalScore >= 80) {
+    console.log('A grade');
+}
+else if (totalScore >= 60) {
+    console.log('B grade');
+}
+else if (totalScore >= 50) {
+    console.log('C grade');
+}
+else if (totalScore >= 40) {
+    console.log('D grade');
+}
+else {
+    console.log('fell Grade');
+}
