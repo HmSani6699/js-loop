1c5e9600e6e50efebb41e10ccf60a5001fdb6ae9
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-28 18:41:08 +0600

    ad to problem practic part

---
+++ practice.js
@@ -0,0 +1,43 @@
+var items = [
+    '1. রাত ৮ টা বাজে মডিউল আনলক করো',
+
+    '2. ফটাফট ভিডিও দেখে দেখে প্রাকটিস করো',
+
+    '3. ভিডিও দেখতে দেখতে নোটস নাও ',
+
+    '4. মডিউল শেষ হলে পুরা মডিউল নিজে নিজে প্রাকটিস করো ',
+
+    '5. কোন কিছু বুঝতে না পারলে (চিন্তা করে দেখো এইখানে কিন্তু একটা শর্ত আছে ), সাপোর্ট সেশনে জয়েন করো '
+];
+
+var itemsLength = items.length
+
+
+for (var i = 0; i < itemsLength; i++) {
+    var item = items[i];
+    //     console.log(item)
+}
+
+var num = 0;
+while (num > itemsLength) {
+    // console.log(items[num]);
+    num++;
+}
+
+
+var num = 5;
+while (num >= itemsLength) {
+    // console.log(items[num]);
+    num--;
+}
+
+// var number = 10;
+while (itemsLength >= 0) {
+    // console.log(items[itemsLength]);
+    itemsLength--;
+}
+
+for (var i = 5; i >= 0; i--) {
+    var item = items[i]
+    // console.log(item)
+}
